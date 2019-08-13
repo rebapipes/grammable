097490e41b382eda750809f5308f390cd54adbca
Reba Pipes <rebablackbox@gmail.com>
2019-08-13 23:25:59 +0000

    added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ApplicationRecord
+  
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
+  end
+
 end
