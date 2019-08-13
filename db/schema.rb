85123826b5a7a71ed8791a2ed37da964e5cd2492
Reba Pipes <rebablackbox@gmail.com>
2019-08-13 17:49:40 +0000

    connecting grams with users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_172240) do
+ActiveRecord::Schema.define(version: 2019_08_13_173630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_08_13_172240) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_grams_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
